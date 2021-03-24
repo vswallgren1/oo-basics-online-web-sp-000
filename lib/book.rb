5e48a13759e83ab2d68b1fa7eec07689cf6fe686
Vince W <vincewallgren@me.com>
2021-03-24 03:32:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 class Book
+  attr_accessor :author, :page_count, :genre
 
   def initialize(title)
     @title = title
@@ -6,30 +7,6 @@ class Book
 
   def title
     @title
-  end
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
   end
 
   def turn_page
