75e2f8152dc97a1318872d7f8b62c8a21c1fc775
Vince W <vincewallgren@me.com>
2021-03-24 03:27:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,19 +1,27 @@
 class Book
-
+ 
   def initialize(title)
     @title = title
   end
-
+ 
   def title
     @title
   end
-
+ 
   def author=(author)
     @author = author
   end
-
+ 
   def author
     @author
   end
-
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
 end
