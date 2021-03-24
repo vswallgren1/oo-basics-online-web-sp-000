5044d00921d2c525b450f823a4923c7006b6c35e
Vince W <vincewallgren@me.com>
2021-03-24 03:56:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -10,38 +10,6 @@ class Shoe
     @brand
   end
 
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def size=(size)
-    @size = size
-  end
-
-  def size
-    @size
-  end
-
-  def material=(material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
-
-  def condition=(condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
-  end
-
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
